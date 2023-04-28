d9246856bca40b66017b6fae90429be542abfd14
mohammedthayyib <mohammedthayyibdev@gmail.com>
2023-04-28 22:40:40 +0530

    started using express.js

--- fullstackopen2023/index.js
+++ fullstackopen2023/index.js
@@ -1,16 +1,34 @@
+const express = require('express')
+const app = express()
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+  ]
 
-
-
-
-const http = require('http')
-
-const app = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/plain' })
-    response.end('Hello world')
+app.get('/', (request, response) => {
+  response.send('<h1><hello World!> </h1>')
 })
 
 
-const PORT = 3001
+// const app = http.createServer((request, response) => {
+//     response.writeHead(200, {'Content-Type':'application/json'})
+//     response.end(JSON.stringify(notes))
+// })
+
+const PORT = 8080
 app.listen(PORT)
 console.log(`server running on port ${PORT}`)
 
