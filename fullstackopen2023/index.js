c568553524bfc2fc80196f72a9a12edb094396c3
mohammedthayyib <mohammedthayyibdev@gmail.com>
2023-05-01 21:28:30 +0530

    server now able to handle http post requests

--- fullstackopen2023/index.js
+++ fullstackopen2023/index.js
@@ -15,13 +15,63 @@ let notes = [
       id: 3,
       content: "GET and POST are the most important methods of HTTP protocol",
       important: true
+    },
+    {
+      id: 4,
+      content: "new content",
+      important: true
     }
   ]
+
+const generateId = () => {
+  const maxId = notes.length > 0 ?
+   Math.max(...notes.map(n => n.id)): 0
+  return maxId + 1
+}
+
+app.use(express.json())
+app.post('/api/notes', (request, response) => {
+  // console.log(request.get('content-type'));
+  // console.log(request.headers);
+    
+  const body = request.body//console.log(body);
+  if(!body.content){
+    return response.status(400).json({error: 'content missing'})
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId()
+  }
+  notes = notes.concat(note)
+  response.json(note)
+})
 
 app.get('/', (request, response) => {
   response.send('<h1><hello World!> </h1>')
 })
 
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  // console.log(note);
+  if(note){
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+  response.status(204).end()
+})
 
 // const app = http.createServer((request, response) => {
 //     response.writeHead(200, {'Content-Type':'application/json'})
